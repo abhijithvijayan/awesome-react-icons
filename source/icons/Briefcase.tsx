9720cb781e541c1d0a193644dcb46cc94c75a56c
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-11-07 20:54:14 +0530

    fix: briefcase icon inner <rect /> size

--- source/icons/Briefcase.tsx
+++ source/icons/Briefcase.tsx
@@ -21,7 +21,7 @@ const Briefcase: React.FC<IconProps> = ({
         className="briefcase_svg__feather briefcase_svg__feather-briefcase"
         {...rest}
       >
-        <rect x={2} y={7} width={size} height={14} rx={2} ry={2} />
+        <rect x={2} y={7} width={20} height={14} rx={2} ry={2} />
         <path d="M16 21V5a2 2 0 00-2-2h-4a2 2 0 00-2 2v16" />
       </svg>
     </>
