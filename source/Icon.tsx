9a5fb424372dce94045ce960ed9257d5c1fda22f
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-11-07 21:23:00 +0530

    refactor: use syntethic default imports for react namespace

--- source/Icon.tsx
+++ source/Icon.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading, @typescript-eslint/no-explicit-any */
-import React from 'react';
+import * as React from 'react';
 
 import CornerRightDownIcon from './icons/CornerRightDown';
 import CloudLightningIcon from './icons/CloudLightning';
